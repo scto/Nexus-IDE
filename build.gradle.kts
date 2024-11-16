f0b6420f224525f4b0d70acd261c2be85f7dd921
scto <tschmid35@gmail.com>
2024-11-16 13:14:22 +0100

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,44 @@ plugins {
      alias(libs.plugins.kotlin.android) apply false
      alias(libs.plugins.kotlin.jvm) apply false
      alias(libs.plugins.materialthemebuilder) apply false
+     id("org.openrewrite.rewrite") version "6.26.0"
+}
+
+dependencies {
+    rewrite(platform("org.openrewrite.recipe:rewrite-recipe-bom:2.21.0"))
+    //rewrite("org.openrewrite.recipe:rewrite-testing-frameworks")
+    //rewrite("org.openrewrite.recipe:rewrite-spring")
+    //rewrite("org.openrewrite.recipe:rewrite-migrate-java")
+    //rewrite("org.openrewrite.recipe:rewrite-static-analysis")
+}
+
+rewrite {
+    activeRecipe(
+//        "com.github.scto.ChangeGradleFiles",
+//        "com.github.scto.ChangeXmlFiles",
+//        "com.github.scto.ChangePackage",
+//        "com.github.scto.UpgradeAndroidGradlePluginVersion",
+//
+//        "org.openrewrite.java.testing.junit5.JUnit4to5Migration",
+//        "org.openrewrite.java.spring.boot3.UpgradeSpringBoot_3_3",
+//        "org.openrewrite.java.migrate.guava.NoGuava",
+//        "org.openrewrite.java.testing.hamcrest.MigrateHamcrestToAssertJ",
+//        "org.openrewrite.staticanalysis.CommonStaticAnalysis",
+    )
+    exclusion(
+        // Excludes a particular yaml file
+        //"subproject-a/src/main/resources/generated.yaml",
+        // Exclude all json files
+        //"**/*.json"
+        //"**/*.xml"
+    )
+    
+    //plainTextMask("**/*.txt")
+    
+    // These are default values, shown for example. It isn't necessary to supply these values manually:
+    configFile = project.getRootProject().file("rewrite.yml")
+    failOnDryRunResults = false
+    sizeThresholdMb = 10
 }
 
 tasks.register<Delete>("clean") {
